717b06027fb027a65bde1e5364c1499a9ba0dfbc
mdsiamkazi <mdsiamkazibd@gmail.com>
2026-05-29 22:24:57 +0600

    authf client url change

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,7 +3,7 @@ import { jwtClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000",
+    baseURL: "http://assignment-9-bvgd.vercel.app",
     fetchOptions: {
     credentials: "include",
      plugins: [
