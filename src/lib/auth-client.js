06ca1585f8830c16b821e1f6e896f9cb13905b85
mdsiamkazi <mdsiamkazibd@gmail.com>
2026-05-29 23:30:57 +0600

    froted url repair

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,7 +3,7 @@ import { jwtClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://assignment-9-bvgd.vercel.app/",
+    baseURL: "https://assignment-9-bvgd.vercel.app",
     fetchOptions: {
     credentials: "include",
      plugins: [
